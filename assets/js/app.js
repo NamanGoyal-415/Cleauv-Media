6ade5df6693093f8357e0c15ebdb334991cd49e9
NamanGoyal-415 <namangoyal77777@gmail.com>
2023-10-26 20:14:41 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,147 @@
+
+$('.S2-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    speedAutoplay: 2000,
+    arrow: false,
+    responsive: [
+        {
+            breakpoint: 9999,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1.2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+// S3-slider
+$('.S3-slider').slick({
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '400px',
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1350,
+            settings: {
+                centerMode: true,
+                centerPadding: '300px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                centerMode: true,
+                centerPadding: '300px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                centerMode: true,
+                centerPadding: '200px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+}
+);
+
+//S7-slider
+$('.S7-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1.7,
+    slidesToScroll: 1,
+    autoplay: true,
+    speedAutoplay: 2000,
+    responsive: [
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 650,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
